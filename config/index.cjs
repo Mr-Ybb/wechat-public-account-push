33e49dbfafdae82f5071f42c6cba0e8f4f886622
Mr-Ybb <811874677@qq.com>
2022-10-17 17:01:40 +0800

    v2.0

--- config/index.cjs
+++ config/index.cjs
@@ -7,6 +7,8 @@
  */
 const USER_CONFIG = {
 
+  USE_PASSAGE: 'wechat-test',
+
   // 使用微信测试号：公众号APP_ID
   APP_ID: 'wx437f394de812cb0f',
 
@@ -48,6 +50,66 @@ const USER_CONFIG = {
       ],
     },
   ],
+
+  // 功能开关,打开：true，关闭：false
+  SWITCH: {
+    /** 每日天气 */
+    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    weather: true,
+
+    /** 节假日 */
+    // 下一休息日综合提醒, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    holidaytts: true,
+
+    /** 每日N句 */
+    // 金山每日一句, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    CIBA: true,
+
+    // 每日一言, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    oneTalk: true,
+
+    // 土味情话(彩虹屁), 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    earthyLoveWords: false,
+
+    // 朋友圈文案, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    momentCopyrighting: false,
+
+    // 毒鸡汤, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    poisonChickenSoup: false,
+
+    // 古诗古文, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    poetry: false,
+
+    /** 星座运势 */
+    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    horoscope: true,
+
+    /** 生日消息和节日消息 */
+    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    birthdayMessage: true,
+
+    /** 学生课表 */
+    // 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    courseSchedule: false,
+  },
+
+  TIAN_API: {
+    // 天行API KEY，如果使用天行API则需要填写此项
+    key: 'a3daedc486c83052988f36b048a113a2',
+
+    /** 天行API相关，需要config中配置 TIAN_API_KEY  */
+    // 早安心语, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    morningGreeting: true,
+
+    // 晚安心语, 填 false 则不使用，按需关闭不使用的功能可以提高运行速度
+    eveningGreeting: true,
+
+    // 天行天气（展示未来N天，最多7天）, 填 0 则不使用，按需关闭不使用的功能可以提高运行速度
+    weather: 0,
+
+    // 全网热搜榜（展示N条，最多30条）, 填 0 则不使用，按需关闭不使用的功能可以提高运行速度
+    networkHot: 0,
+  },
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
--- docs/message-channel/test-number.md
+++ docs/message-channel/test-number.md
@@ -34,12 +34,12 @@
 
 ```
 {{date.DATA}} 
-城市：{{city.DATA}} 
+城市：{{province.DATA}}-{{city.DATA}} 
 天气：{{weather.DATA}} 
 最低气温: {{min_temperature.DATA}} 
 最高气温: {{max_temperature.DATA}} 
+天秤座运势：{{comprehensive_horoscope.DATA}}
 今天是我们恋爱的第{{love_day.DATA}}天
-今天是我们结婚的第{{marry_day.DATA}}天
 
 {{birthday_message.DATA}}
 
